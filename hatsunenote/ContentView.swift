176b2a7d5905d215593f0a4cc7a5d8a5fe0da06a
Kurisu <makise_kurisuu@outlook.jp>
2020-10-19 09:16:32 +0800

    Initial Commit

---
+++ hatsunenote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hatsunenote
+//
+//  Created by Kurisu on 2020/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hatsunenote/hatsunenoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  hatsunenoteApp.swift
+//  hatsunenote
+//
+//  Created by Kurisu on 2020/10/19.
+//
+
+import SwiftUI
+
+@main
+struct hatsunenoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
